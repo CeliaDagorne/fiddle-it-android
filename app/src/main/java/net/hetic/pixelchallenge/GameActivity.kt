5cc3870b590cd22e2d36ddf0ef8bb27bc9dd6c98
Marine LB <marine.leborgne@live.fr>
2018-05-20 19:11:19 +0200

    :sparkles: display user word in view

--- app/src/main/java/net/hetic/pixelchallenge/GameActivity.kt
+++ app/src/main/java/net/hetic/pixelchallenge/GameActivity.kt
@@ -3,6 +3,7 @@ package net.hetic.pixelchallenge
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast;
 
 
@@ -14,21 +15,23 @@ class GameActivity : AppCompatActivity() {
 
         val letter_btn = findViewById(R.id.letterBtn) as Button
         val letter_btn_2 = findViewById(R.id.letterBtn2) as Button
-        val user_word = mutableListOf<String>()
+        val user_word_view = findViewById(R.id.textView) as TextView
+        var user_word = ""
 
         letter_btn.setOnClickListener {
             val letter_btn_string = letter_btn.text.toString()
-            user_word.add(letter_btn_string)
+            user_word += letter_btn_string
             val user_word_txt = user_word.toString()
-            Toast.makeText(this@GameActivity, user_word_txt, Toast.LENGTH_SHORT).show()
-
+            // Toast.makeText(this@GameActivity, user_word_txt, Toast.LENGTH_SHORT).show()
+            user_word_view.text = user_word_txt
         }
 
         letter_btn_2.setOnClickListener {
             val letter_btn_string = letter_btn_2.text.toString()
-            user_word.add(letter_btn_string)
+            user_word += letter_btn_string
             val user_word_txt = user_word.toString()
-            Toast.makeText(this@GameActivity, user_word_txt, Toast.LENGTH_SHORT).show()
+            // Toast.makeText(this@GameActivity, user_word_txt, Toast.LENGTH_SHORT).show()
+            user_word_view.text = user_word_txt
         }
 
 
