74c89df4c47388f913a0947ca7ec6c66f1bac672
Marine LB <marine.leborgne@live.fr>
2018-05-24 15:52:12 +0200

    :sparkles: add buttons programmatically depending on user_word

--- app/src/main/java/net/hetic/pixelchallenge/GameActivity.kt
+++ app/src/main/java/net/hetic/pixelchallenge/GameActivity.kt
@@ -5,8 +5,16 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.Toast
+import android.graphics.Color
+import android.R.attr.button
+import android.support.design.widget.TabLayout.GRAVITY_CENTER
 
+// TODO : changer le LinearLayout en un truc qui permet de wrap quand y'a trop de lettres
+// TODO : shuffle les button avant de les ajouter a la vue
+// TODO : ajouter des lettres en plus pour toujours en avoir un certain nombre
+// (à définir mais genre 12?)
 
 class GameActivity : AppCompatActivity()  {
 
@@ -14,12 +22,14 @@ class GameActivity : AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        val letter_btn = findViewById(R.id.letterBtn) as Button
-        val letter_btn_2 = findViewById(R.id.letterBtn2) as Button
+        val buttonContainer = findViewById(R.id.buttonContainer) as LinearLayout
+
+        // val letter_btn = findViewById(R.id.letterBtn) as Button
+        // val letter_btn_2 = findViewById(R.id.letterBtn2) as Button
         val user_word_view = findViewById(R.id.textView) as TextView
         val underscore_view = findViewById(R.id.underscore) as TextView
 
-        val word_to_find = "ARARA"
+        val word_to_find = "CELIABLABLASALUT"
         var word_to_find_formatted = ""
         val word_to_find_length = (word_to_find.length * 2 ) - 1
 
@@ -29,45 +39,48 @@ class GameActivity : AppCompatActivity()  {
         for (letter in word_to_find) {
             word_to_find_formatted += "$letter "
             underscores += "_ "
+
+            val button = Button(this)
+            val scale = resources.displayMetrics.density
+            val dpWidthInPx = (40 * scale).toInt()
+            val dpHeightInPx = (40 * scale).toInt()
+            val dpMarginInPx = (8 * scale).toInt()
+
+            button.text = "$letter"
+            button.setBackgroundResource(R.drawable.cta_bg)
+            button.setTextColor(Color.WHITE)
+            button.setGravity(GRAVITY_CENTER)
+            button.layoutParams = LinearLayout.LayoutParams(dpWidthInPx, dpHeightInPx)
+            val param = button.layoutParams as LinearLayout.LayoutParams
+            param.setMargins(dpMarginInPx,dpMarginInPx,dpMarginInPx,dpMarginInPx);
+            button.layoutParams = param
+
+            // TODO : ajouter un eventListener sur le click de tous les boutons,
+            // qui récupère la valeur cliquée et check si le mot est terminé ou pas
+
+            button.setOnClickListener {
+                val letter_btn_string = button.text.toString()
+                if (user_word.length < word_to_find_formatted.length) {
+                    user_word += letter_btn_string+" "
+                    // Toast.makeText(this@GameActivity, user_word_txt, Toast.LENGTH_SHORT).show()
+                    user_word_view.text = user_word
+
+                    if(user_word == word_to_find_formatted) {
+                        Toast.makeText(this@GameActivity, "found $word_to_find", Toast.LENGTH_SHORT).show()
+                    }else if (user_word.length == word_to_find_formatted.length) {
+                        Toast.makeText(this@GameActivity, "nope !", Toast.LENGTH_SHORT).show()
+                    }
+
+                }else{
+                    Toast.makeText(this@GameActivity, "too long", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            buttonContainer.addView(button)
+
+
         }
         underscore_view.text = underscores
-
-
-        letter_btn.setOnClickListener {
-            val letter_btn_string = letter_btn.text.toString()
-            if (user_word.length < word_to_find_formatted.length) {
-                user_word += letter_btn_string+" "
-                // Toast.makeText(this@GameActivity, user_word_txt, Toast.LENGTH_SHORT).show()
-                user_word_view.text = user_word
-
-                if(user_word == word_to_find_formatted) {
-                    Toast.makeText(this@GameActivity, "found $word_to_find", Toast.LENGTH_SHORT).show()
-                }else if (user_word.length == word_to_find_formatted.length) {
-                    Toast.makeText(this@GameActivity, "nope !", Toast.LENGTH_SHORT).show()
-                }
-
-            }else{
-                Toast.makeText(this@GameActivity, "too long", Toast.LENGTH_SHORT).show()
-            }
-
-        }
-
-        letter_btn_2.setOnClickListener {
-            val letter_btn_string = letter_btn_2.text.toString()
-            if (user_word.length < word_to_find_formatted.length) {
-                user_word += letter_btn_string+" "
-                // Toast.makeText(this@GameActivity, user_word_txt, Toast.LENGTH_SHORT).show()
-                user_word_view.text = user_word
-
-                if(user_word == word_to_find_formatted) {
-                    Toast.makeText(this@GameActivity, "found $word_to_find", Toast.LENGTH_SHORT).show()
-                }else if (user_word.length == word_to_find_formatted.length) {
-                    Toast.makeText(this@GameActivity, "nope !", Toast.LENGTH_SHORT).show()
-                }
-            }else{
-                Toast.makeText(this@GameActivity, "too long", Toast.LENGTH_SHORT).show()
-            }
-        }
 
 
     }
